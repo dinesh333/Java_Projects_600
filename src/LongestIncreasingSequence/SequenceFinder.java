e8b914fa48f11e380cb07ab284c9789c08e4315e
dinesh333 <dinesh123basnet@gmail.com>
2017-03-12 06:05:54 -0700

    Used stack and recursion to find longest increasing sequence

--- src/LongestIncreasingSequence/SequenceFinder.java
+++ src/LongestIncreasingSequence/SequenceFinder.java
@@ -1,21 +1,17 @@
 package LongestIncreasingSequence;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*; 
+import java.util.*; 
 
 public class SequenceFinder {
-	
-	final static int ROWS = 4 + 2; //+2 for 2 extra rows of 0s.
-	final static int COLUMNS = 4 + 2; //+2 for 2 extra columns of 0s.
-	// x and y coordinates for 8 directions, starting from North
-	static int[][] paddedMatrix;
+	final static int ROWS = 15 + 2; 
+	final static int COLUMNS = 15 + 2; 
+	final static int[] DIRECTIONS = {-1,0,-1,1,0,1,1,1,1,0,1,-1,0,-1,-1,-1}; //N,NE,E,SE,S,SW,W,NW
+	static int[][] paddedMatrix; 
+	static ArrayList<Integer> maxLength; 
 	
 	public static void main(String[] args){
-		paddedMatrix = getMatrix("sampledata.txt");
-		//printMatrix(paddedMatrix);
+		paddedMatrix = getMatrix("testdataHW3.txt");
 		getLongestSequence();	
 	}
 
@@ -31,6 +27,7 @@ public class SequenceFinder {
 			}
 		}
 		
+		//To get 2D array from file
 		File matrixFile = new File(fileWithMatrix);
 		Scanner scan = null;
 		
@@ -52,21 +49,15 @@ public class SequenceFinder {
 		return matrix;
 	}
 	
-	public static void printMatrix(ArrayList<Integer>[][] matrix) {
-		for(int i = 0; i < ROWS; i++){
-			for(int j = 0; j < COLUMNS; j++){
-				System.out.print(String.format("%1$6s", matrix[i][j]));
-			}
-			System.out.println("");
-		}
-	}
-	
 	/*
-	 * For each number in matrix, and find out longest sequence if you start
-	 * from that number.
+	 * Get every possible increasing sequence starting from every possible
+	 * starting number. If the length of a particular sequence is larger than
+	 * the lengths of other sequences before it, assign the sequence to
+	 * maxLength.
 	 */
 	public static void getLongestSequence() {
-		ArrayList<Integer>[][] longestPaths = new ArrayList[ROWS][COLUMNS]; 
+		maxLength = new ArrayList<>();	
+		maxLength.add(1);
 		for(int row = 1; row < ROWS-1; row++){
 			for(int col = 1; col < COLUMNS-1; col++){
 				Stack st = new Stack();
@@ -74,39 +65,57 @@ public class SequenceFinder {
 				startNumAndPos.add(paddedMatrix[row][col]);
 				startNumAndPos.add(row);
 				startNumAndPos.add(col);
-				longestPaths[row][col] = findLongestPathFor(startNumAndPos, st);
+				findLongestPathFor(startNumAndPos, st);
 			}
 		}
-		printMatrix(longestPaths);
+		
+		//Print sequence information of maximum length
+		for(int i = 0; i < (maxLength.size()/3); i++){
+			int value = i*3;
+			int row = value+1;
+			int col = value+2;
+			value = maxLength.get(value);
+			row = maxLength.get(row)-1;
+			col = maxLength.get(col)-1;
+			System.out.println(value + " (" + row + ", " + col + ")");
+		}
+		System.out.println("The length of the sequence is: " + (maxLength.size()/3));
 	}
 	
 	public static ArrayList<Integer> findLongestPathFor(ArrayList<Integer> startNumAndPos, Stack st) {
-		System.out.println(startNumAndPos); //Delete later
-		ArrayList<Integer> surroundingNums = getSurroundingNums(startNumAndPos); 
-		
+		ArrayList<Integer> surroundingNums = getSurroundingNums(startNumAndPos);
+		int startSize = startNumAndPos.size();
+		boolean found = false;
 		for(int i = 0; i < 24; i++){
-			if(i % 3 == 0 && startNumAndPos.get(0) < surroundingNums.get(i)){
-				ArrayList<Integer> sequence = startNumAndPos;
+			if(i % 3 == 0 && startNumAndPos.get(startSize-3) < surroundingNums.get(i)){
+				found = true;
+				ArrayList<Integer> sequence = new ArrayList<Integer>();
 				sequence.add(surroundingNums.get(i));
 				sequence.add(surroundingNums.get(i+1));
 				sequence.add(surroundingNums.get(i+2));
-				st.push(sequence);
-				System.out.println(surroundingNums); //Delete later
+				
+				ArrayList<Integer> newList = new ArrayList<Integer>();
+				newList.addAll(startNumAndPos);
+				newList.addAll(sequence);
+				
+				st.push(newList);
 			}
 		}
-		System.out.println("------------------------------------------------");
 		
-		if(st.empty()){ //base case
-			return startNumAndPos;
-		} else { //recursive case
-			return startNumAndPos; //will be changed
+		if(!st.empty()){
+			ArrayList<Integer> top = (ArrayList<Integer>) st.pop();
+			if(top.size() > maxLength.size())
+				maxLength = top;
+			return findLongestPathFor(top, st);
 		}
+		return startNumAndPos;
 	}
 	
-	//Returns ArrayList of surrounding numbers with their positions: 8 + 2 * 8 = 24 total elements
-	private static ArrayList<Integer> getSurroundingNums(ArrayList<Integer> startNumAndPos) {
-		int row = startNumAndPos.get(1);
-		int col = startNumAndPos.get(2);
+	//Returns ArrayList of surrounding numbers with their positions: 8 + 2 * 8 = 24 total elements returned
+	public static ArrayList<Integer> getSurroundingNums(ArrayList<Integer> startNumAndPos) {
+		int startSize = startNumAndPos.size();
+		int row = startNumAndPos.get(startSize-2);
+		int col = startNumAndPos.get(startSize-1);
 		ArrayList<Integer> allSurroundingNums = new ArrayList<>();
 		for(int i = 0; i <= 7; i++){
 			int new_row = row + (DIRECTIONS[2*i]);
