75af4783776424ca42f20dc12708875d8fba0335
dinesh333 <dinesh123basnet@gmail.com>
2017-04-23 14:15:37 -0700

    Added delete method to AVLTree class

--- src/AVLTree/AVLTreeTest.java
+++ src/AVLTree/AVLTreeTest.java
@@ -37,7 +37,7 @@ class AVLTree{
 		} else if(current.data < data){
 			current.right = insert(data, current.right);
 		} else {
-			//current.data == data
+			//data already exists in tree.
 			return current;
 		}
 		current.height = maxHeight(getHeight(current.left), getHeight(current.right)) + 1;
@@ -103,5 +103,78 @@ class AVLTree{
 		a.height = maxHeight(getHeight(a.left), getHeight(a.right)) + 1;
 		b.height = maxHeight(getHeight(b.left), getHeight(b.right)) + 1;
 		return b;
+	}
+	
+	public void delete(int data){
+		if(root == null)
+			System.out.println("AVL Tree is empty.");
+		else 
+			root = delete(data, root);
+	}
+	
+	private AVLNode delete(int data, AVLNode current){
+		if(current == null){
+			return null;
+		}else if(current.data > data){
+			current.left = delete(data, current.left);
+		}else if(current.data < data){
+			current.right = delete(data, current.right);
+		}else{
+			//current node has the data to be deleted
+			
+			//current node has no children
+			if(current.left == null && current.right == null){
+				return null;
+			}
+			//current node has right child only
+			else if(current.left == null && current.right != null){
+				return current.right;
+			}
+			//current node has left child only
+			else if(current.left != null && current.right == null){
+				return current.left;
+			}
+			//current node has left and right children
+			else{
+				//find node with largest data on left subtree of current node.
+				AVLNode replacingNode = findNodeWithLargest(current.left);
+				current.data = replacingNode.data;
+				current.left = delete(replacingNode.data, current.left);
+			}
+		}
+		
+		current.height = maxHeight(getHeight(current.left), getHeight(current.right)) + 1;
+		
+		//Check if tree unbalanced
+		int balanceCheck = getHeight(current.left) - getHeight(current.right);
+		int leftSubtreeBalanceCheck = getHeight(current.left.left) - getHeight(current.left.right);
+		int rightSubtreeBalanceCheck = getHeight(current.right.left) - getHeight(current.right.right);
+		
+		//Case 1: Left-Left => Single rotation to right.
+		if(balanceCheck > 1 && leftSubtreeBalanceCheck >= 0){
+			current = rotateNodeToRight(current);
+		}
+		//Case 2: Left-Right => Single rotation to left. Single rotation to right.
+		else if(balanceCheck > 1 && leftSubtreeBalanceCheck < 0){
+			current.left = rotateNodeToLeft(current.left);
+			current = rotateNodeToRight(current);
+		}
+		//Case 3: Right-left => Single rotation to right. Single rotation to left.
+		else if(balanceCheck < -1 && rightSubtreeBalanceCheck >= 0){
+			current.right = rotateNodeToRight(current.right);
+			current = rotateNodeToLeft(current);
+		}
+		//Case 4: Right-Right case => Single rotation to left.
+		else if(balanceCheck < -1 && rightSubtreeBalanceCheck < 0){
+			current = rotateNodeToLeft(current);
+		}
+		
+		return current;
+	}
+	
+	private AVLNode findNodeWithLargest(AVLNode current){
+		while(current.right != null)
+			current = current.right;
+		return current;
 	}
 }
